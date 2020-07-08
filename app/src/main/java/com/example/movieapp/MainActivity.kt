f643fef4ffb299bdaff757b0b7233fc7d03d07e4
Benjamin Vouillon <benjivou2@gmail.com>
2020-07-08 17:10:30 +0200

    + retrieve list of movies from server done

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,15 +2,25 @@ package com.example.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import com.example.movieapp.viewModel.MainViewModel
+
+import androidx.lifecycle.ViewModelProviders
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Picasso.get().load("http://image.tmdb.org/t/p/w185/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg").into(imageView);
+        // Connecte the ViewModel to the app
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        //Picasso.get().load("http://image.tmdb.org/t/p/w185/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg").into(imageView);
 
     }
 }
