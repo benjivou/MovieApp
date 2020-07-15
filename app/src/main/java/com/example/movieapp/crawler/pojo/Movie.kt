f527bce092ddbd703a6f4e90e1d1c130a9848a73
Benjamin Vouillon <benjivou2@gmail.com>
2020-07-15 10:34:27 +0200

    + convert Movie to an Entity class + add a field Image to the class Movie it type is Bitmap?

--- app/src/main/java/com/example/movieapp/crawler/pojo/Movie.kt
+++ app/src/main/java/com/example/movieapp/crawler/pojo/Movie.kt
@@ -1,24 +1,30 @@
 package com.example.movieapp.crawler.pojo
 
+import android.graphics.Bitmap
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by Benjamin Vouillon on 08,July,2020
  */
+@Entity
 data class Movie(
-    @SerializedName("popularity") val popularity: Float,
-    @SerializedName("vote_pount") val votePount: Int,
-    @SerializedName("video") val video: Boolean,
-    @SerializedName("poster_path") val posterPath: String,
-    @SerializedName("id") val id: Int,
-    @SerializedName("adult") val adult: Boolean,
-    @SerializedName("backdrop_path") val backdropPath: String,
-    @SerializedName("original_language") val originalLanguage: String,
-    @SerializedName("original_title") val originalTitle: String,
-    @SerializedName("genreIds") val genreIds: List<Int>?,
-    @SerializedName("title") val title: String,
-    @SerializedName("vote_average") val voteAverage: Float,
-    @SerializedName("overview") val overview: String,
-    @SerializedName("release_date") val releaseDate: String
+    @PrimaryKey @ColumnInfo(name = "id") @SerializedName("id") val id: Int,
+    @ColumnInfo(name = "popularity") @SerializedName("popularity") val popularity: Float,
+    @ColumnInfo(name = "vote_pount") @SerializedName("vote_pount") val votePount: Int,
+    @ColumnInfo(name = "video") @SerializedName("video") val video: Boolean,
+    @ColumnInfo(name = "poster_path") @SerializedName("poster_path") val posterPath: String,
+    @ColumnInfo(name = "adult") @SerializedName("adult") val adult: Boolean,
+    @ColumnInfo(name = "backdrop_path") @SerializedName("backdrop_path") val backdropPath: String,
+    @ColumnInfo(name = "original_language") @SerializedName("original_language") val originalLanguage: String,
+    @ColumnInfo(name = "original_title") @SerializedName("original_title") val originalTitle: String,
+    @ColumnInfo(name = "genreIds") @SerializedName("genreIds") val genreIds: List<Int>?,
+    @ColumnInfo(name = "title") @SerializedName("title") val title: String,
+    @ColumnInfo(name = "vote_average") @SerializedName("vote_average") val voteAverage: Float,
+    @ColumnInfo(name = "overview") @SerializedName("overview") val overview: String,
+    @ColumnInfo(name = "release_date") @SerializedName("release_date") val releaseDate: String,
+    @ColumnInfo(name = "image") @SerializedName("image") var Image: Bitmap?
 
 )
--- app/src/main/java/com/example/movieapp/likesmanager/MoviesEntity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.movieapp.likesmanager
-
-/**
- * Created by Benjamin Vouillon on 15,July,2020
- */
-class MoviesEntity {
-
-}
