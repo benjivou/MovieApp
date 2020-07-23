163fc2252d9e156315d5b5b06b417233c59f9dd5
Benjamin Vouillon <benjivou2@gmail.com>
2020-07-23 08:18:57 +0200

    + change internet callback to livedata

--- app/src/main/java/com/example/movieapp/crawler/LiveDataCallAdapter.kt
+++ app/src/main/java/com/example/movieapp/crawler/LiveDataCallAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.movieapp.crawler
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import retrofit2.Call
 import retrofit2.CallAdapter
 import retrofit2.Callback
@@ -11,34 +12,40 @@ import java.util.concurrent.atomic.AtomicBoolean
 /**
  * Created by Benjamin Vouillon on 17,July,2020
  */
-class LiveDataCallAdapter<ResultPage>(private val responseType: Type) :
-    CallAdapter<ResultPage, LiveData<ResultPage>> {
 
-    override fun responseType(): Type {
-        return responseType
-    }
-
-    override fun adapt(call: Call<ResultPage>): LiveData<ResultPage> {
-        return object : LiveData<ResultPage>() {
-            internal var started = AtomicBoolean(false)
+class LiveDataCallAdapter<R>(private val responseType: Type): CallAdapter<R, LiveData<ApiResponse<R>>> {
+    override fun adapt(call: Call<R>): MutableLiveData<ApiResponse<R>> {
+        return object : MutableLiveData<ApiResponse<R>>() {
+            private var isSuccess = false
 
             override fun onActive() {
                 super.onActive()
-                if (started.compareAndSet(false, true)) {
-                    call.enqueue(object : Callback<ResultPage> {
-                        override fun onFailure(call: Call<ResultPage>, t: Throwable) {
-                            TODO()
-                        }
+                if (!isSuccess) enqueue()
+            }
 
-                        override fun onResponse(
-                            call: Call<ResultPage>,
-                            response: Response<ResultPage>
-                        ) {
-                            postValue(response.body())
-                        }
-                    })
-                }
+            override fun onInactive() {
+                super.onInactive()
+                dequeue()
+            }
+
+            private fun dequeue() {
+                if (call.isExecuted) call.cancel()
+            }
+
+            private fun enqueue() {
+                call.enqueue(object : Callback<R> {
+                    override fun onFailure(call: Call<R>, t: Throwable) {
+                        postValue(ApiResponse.create(UNKNOWN_CODE, t))
+                    }
+
+                    override fun onResponse(call: Call<R>, response: Response<R>) {
+                        postValue(ApiResponse.create(response))
+                        isSuccess = true
+                    }
+                })
             }
         }
     }
+
+    override fun responseType(): Type = responseType
 }
--- app/src/main/java/com/example/movieapp/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/movieapp/viewModel/MainViewModel.kt
@@ -5,21 +5,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import com.example.movieapp.crawler.MoviesService
-import com.example.movieapp.crawler.TypeDisplay
+import com.example.movieapp.crawler.*
 import com.example.movieapp.crawler.pojo.Movie
-import com.example.movieapp.crawler.pojo.ResultPage
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
 
 /**
  * Created by Benjamin Vouillon on 08,July,2020
  */
-
 
 const val URL = "https://api.themoviedb.org/3/movie/"
 
@@ -27,26 +20,17 @@ class MainViewModel : ViewModel() {
     private val TAG = "MainViewModel"
 
     /**
-     * Our 3 lists of Movies
-     */
-    private var currentList: MutableLiveData<List<Movie>> = MutableLiveData()
-
-
-    /**
      * Type of the list displayed
      */
     private var typeDisplay: MutableLiveData<TypeDisplay> =
         MutableLiveData(TypeDisplay.POPULAR)
 
-    init {
-        // connect the current list to the livedata
-        currentList =
-            Transformations.switchMap<TypeDisplay, List<Movie>>(
-                this.typeDisplay as LiveData<TypeDisplay>
-            ) {
-                internetCall()
-            } as MutableLiveData<List<Movie>>
-    }
+    private var currentList: LiveData<List<Movie>> =
+        Transformations.switchMap<TypeDisplay, List<Movie>>(
+            typeDisplay
+        ) {
+            internetCall()
+        }
 
     /*
     return the list of elements necessary
@@ -59,41 +43,24 @@ class MainViewModel : ViewModel() {
         this.typeDisplay.value = typeDisplay
     }
 
-    private fun internetCall(): MutableLiveData<List<Movie>> {
-        val myMutableLiveData = MutableLiveData<List<Movie>>()
+    private fun internetCall(): LiveData<List<Movie>> {
         // prepare the internet call
         val service = Retrofit.Builder()
             .baseUrl(URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(LiveDataCallAdapterFactory())
             .build()
             .create(MoviesService::class.java)
 
-        typeDisplay.value?.s?.let {
-            service.listOfMovies(it).enqueue(object : Callback<ResultPage> {
-                override fun onResponse(call: Call<ResultPage>, response: Response<ResultPage>) {
-                    val allData = response.body()
-                    if (allData != null) {
-                        myMutableLiveData.value = allData.results
-                    } else {
-                        Log.w(
-                            TAG,
-                            "onResponse: connection trouble ( there is the no Data in the response"
-                        )
-                    }
+        return Transformations.map(service.listOfMovies(typeDisplay.value!!.s)) {
+            when (it) {
+                is ApiSuccessResponse -> it.body.results
+                is ApiEmptyResponse -> emptyList()
+                is ApiErrorResponse -> {
+                    Log.w(TAG, "internetCall: " + it.errorMessage)
+                    emptyList()
                 }
-
-                override fun onFailure(call: Call<ResultPage>, t: Throwable) {
-                    Log.e(TAG, "onFailure: ${t.message}")
-                }
-            })
+            }
         }
-        return myMutableLiveData
-
-    }
-
-
-    override fun onCleared() {
-        super.onCleared()
-        typeDisplay.removeObserver { }
     }
 }
